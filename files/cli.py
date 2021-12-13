e5cb364287b68bf48715576777d9bb76f4f80d99
nloomis <noahmmLoomis@gmail.com>
2021-12-13 13:56:32 -0500

    dasd

--- files/cli.py
+++ files/cli.py
@@ -19,7 +19,8 @@ class Cli():
         print(Fore.BLUE + " " .join(self.crypto.alphabet_guessed), end='\n\n\n')
 
     def display_encoded_quote(self):
-        print(" ".join(self.crypto.decoded_quote))
+        # The following line is for testing, to see what your quote is as you're decoding it
+        # print(" ".join(self.crypto.decoded_quote))
         print(Fore.GREEN + " ".join(self.crypto.encoded_quote))
         print(Fore.GREEN + " " .join(self.crypto.quote_guessed), end='\n\n\n')
 
@@ -55,54 +56,54 @@ class Cli():
     def restart_game(self):
         self.crypto = Crypto()
 
+if __name__ == "__main__":
+    cli = Cli()
+    print("\nWelcome to the Cryptographers crypto conundrum, enter q to quit!!!")
+    cli.display_help_menu()
+    while (True):
 
-cli = Cli()
-print("\nWelcome to the Cryptographers crypto conundrum, enter q to quit!!!")
-cli.display_help_menu()
-while (True):
-
-    cli.display_guessed_letters()
-    cli.display_encoded_quote()
-
-    try:
-        from_letter = input("Convert letter: ")
-        cli.handle_input(from_letter)
-
-        to_letter = input("To letter: ")
-        cli.handle_input(to_letter)
-
-        if not cli.is_command(from_letter) and not cli.is_command(to_letter):
-            cli.crypto.guess_letter(from_letter, to_letter)
-
-    except (FixingQuoteException, GiveHintException) as e:
-        print(f'{Fore.BLUE}{e}')
-    except InvalidLetterGuessException as e:
-        print(f'{Fore.RED}{e}')
-
-    except QuitGameException:
-        cli.end_time = perf_counter()
-        play_again = input(
-            f'{Fore.GREEN}Time was {cli.get_time():0.2f} seconds Play again? (Default is n) [y/n] ')
-        if play_again.upper() == "N" or play_again.strip() == "":
-            print(f'{Fore.BLUE}Thanks for playing :)')
-            break
-        else:
-            cli.restart_game()
-    except EndGameException as e:
-        print(f'{Fore.BLUE}{e}')
         cli.display_guessed_letters()
         cli.display_encoded_quote()
 
-        cli.end_time = perf_counter()
-        if cli.crypto.is_game_won():
-            print(f"The quote was: {cli.crypto.decoded_quote}")
+        try:
+            from_letter = input("Convert letter: ")
+            cli.handle_input(from_letter)
+
+            to_letter = input("To letter: ")
+            cli.handle_input(to_letter)
+
+            if not cli.is_command(from_letter) and not cli.is_command(to_letter):
+                cli.crypto.guess_letter(from_letter, to_letter)
+
+        except (FixingQuoteException, GiveHintException) as e:
+            print(f'{Fore.BLUE}{e}')
+        except InvalidLetterGuessException as e:
+            print(f'{Fore.RED}{e}')
+
+        except QuitGameException:
+            cli.end_time = perf_counter()
             play_again = input(
-                f'{Fore.GREEN}You won!!! It took you {cli.get_time():0.2f} seconds. Play again? (Default is y) [y/n] ')
-            print(f"The quote was {cli.crypto.decoded_quote}")
-            if play_again.upper() == "Y" or play_again.strip() == "":
-                cli.restart_game()
-            else:
+                f'{Fore.GREEN}Time was {cli.get_time():0.2f} seconds Play again? (Default is n) [y/n] ')
+            if play_again.upper() == "N" or play_again.strip() == "":
                 print(f'{Fore.BLUE}Thanks for playing :)')
                 break
-        else:
-            print(f'{Fore.RED}You didn\'t guess everything right')
+            else:
+                cli.restart_game()
+        except EndGameException as e:
+            print(f'{Fore.BLUE}{e}')
+            cli.display_guessed_letters()
+            cli.display_encoded_quote()
+
+            cli.end_time = perf_counter()
+            if cli.crypto.is_game_won():
+                print(f"The quote was: {cli.crypto.decoded_quote}")
+                play_again = input(
+                    f'{Fore.GREEN}You won!!! It took you {cli.get_time():0.2f} seconds. Play again? (Default is y) [y/n] ')
+                print(f"The quote was {cli.crypto.decoded_quote}")
+                if play_again.upper() == "Y" or play_again.strip() == "":
+                    cli.restart_game()
+                else:
+                    print(f'{Fore.BLUE}Thanks for playing :)')
+                    break
+            else:
+                print(f'{Fore.RED}You didn\'t guess everything right')
